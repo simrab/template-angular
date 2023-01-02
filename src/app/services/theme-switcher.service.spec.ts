eb67ad26136a94f4b08d4aaaad3663c5759a0595
Massimo Siardi <msiardi7@hotmail.com>
2023-01-02 17:37:16 +0100

    fix(tests services): fix hook, add tests for theme switcher service

--- src/app/services/theme-switcher.service.spec.ts
+++ src/app/services/theme-switcher.service.spec.ts
@@ -4,13 +4,35 @@ import { ThemeSwitcherService } from './theme-switcher.service';
 
 describe('ThemeSwitcherService', () => {
   let service: ThemeSwitcherService;
+  let localStore = {} as Record<string, string>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(ThemeSwitcherService);
+    jest.spyOn(window.localStorage, 'getItem');
+    jest.fn((key: string) => (key in localStore ? localStore[key] : null));
+    jest.spyOn(window.localStorage, 'setItem');
+    jest.fn((key: string, value: string) => (localStore[key] = value + ''));
+    jest.spyOn(window.localStorage, 'clear');
+    jest.fn(() => (localStore = {}));
   });
-
+  afterEach(() => {
+    // restore the spy created with spyOn
+    jest.restoreAllMocks();
+  });
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+  it('should return current theme', () => {
+    service.setTheme('light');
+    expect(service.getTheme()).toBe('light');
+  });
+  it('should set theme to dark', () => {
+    service.setTheme('dark');
+    expect(localStorage.getItem('theme')).toBe('dark');
+  });
+  it('should toggle theme', () => {
+    expect(service.toggleTheme('dark')).toBe('light');
+    expect(service.toggleTheme('light')).toBe('dark');
   });
 });
